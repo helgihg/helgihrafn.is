d93dc3eb390b1738555ae3b4a9e9234598560111
Helgi Hrafn Gunnarsson <helgi@binary.is>
2026-02-25 15:44:30 +0000

    Build script allows future.

--- build-publish-deploy.sh
+++ build-publish-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-bundle exec jekyll build
+bundle exec jekyll build --future
 docker build . --no-cache -t ghcr.io/helgihg/helgihrafn.is:latest
 docker push ghcr.io/helgihg/helgihrafn.is:latest
 kubectl rollout restart deployment/helgihrafn-is -n prod-is-helgihrafn
