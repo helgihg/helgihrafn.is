c90976fd670fca93c34a013fff7f4bf41018e971
Helgi Hrafn Gunnarsson <helgi@binary.is>
2026-08-16 22:15:07 +0000

    K8s: Namespace defined by Helm chart release rather than values file.

--- build-publish-deploy.sh
+++ build-publish-deploy.sh
@@ -3,4 +3,4 @@ set -e
 bundle exec jekyll build --future
 docker build . --no-cache -t ghcr.io/helgihg/helgihrafn.is:latest
 docker push ghcr.io/helgihg/helgihrafn.is:latest
-kubectl rollout restart deployment/helgihrafn-is -n prod-is-helgihrafn
+helm upgrade --install -n prod-helgihrafn --create-namespace helgihrafn deployment
